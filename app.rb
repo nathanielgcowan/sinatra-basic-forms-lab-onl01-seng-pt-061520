3e22d5b68ec237d567c09e709ca8b9169f9250af
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-21 21:18:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
   
   post '/' do
-    @puppy = Puppy.new(params['name'],params['breed'],params['puppy#age'])
+    @puppy = Puppy.new(params['name'],params['breed'],params['months_old'])
     erb :display_puppy
   end
 
