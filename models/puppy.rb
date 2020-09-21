124466a82046c8a8e9d7325a77bd7e6d4a8f2f74
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-21 21:22:02 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,25 +1,5 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
-class Puppy
-  
-   def piglatinize(input_str)
-    input_str.split(" ").map{ |word| piglatinize_word(word)}.join(" ")
-  end
-
-  private
-
-  def vowel?(char)
-    char.match(/[aAeEiIoOuU]/)
-  end
-
-  def piglatinize_word(word)
-    if vowel?(word[0])
-      word + "way"
-    else
-      vowel_index = word.index(/[aAeEiIoOuU]/)
-      consonants = word.slice(0..vowel_index-1)
-      word_leftover = word.slice(vowel_index..word.length)
-      word_leftover + consonants + "ay"
-    end
-  end
+class Puppy 
+  def initialize()
   
 end
